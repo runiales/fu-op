3fc9f16f0a0cf8ffd65a2beab2f80b00d961438d
runiales <irpic.am@gmail.com>
2019-09-05 10:55:22 +0200

    initial commit

---
+++ prediction.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Music21
+
+from music21 import *
+
+
